91f552ecd5ab62947c0601e8013e0b8fb0aab486
Ahtsham Amjad <ahtshamamjad592@gmail.com>
2026-01-01 16:21:18 +0500

    updated the login logic to login from API

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,19 @@
 const { defineConfig } = require('cypress')
-require('dotenv').config()
+require('dotenv').config({ override: true })
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      // Load environment variables into Cypress config
+      config.env = {
+        ...config.env,
+        username: process.env.USERNAME,
+        password: process.env.PASSWORD,
+        AccessToken: process.env.ACCESS_TOKEN,
+      }
+      config.baseUrl = process.env.BASE_URL || config.baseUrl
+      return config
     },
   },
 })
--- cypress/e2e/loginTests.cy.js
+++ cypress/e2e/loginTests.cy.js
@@ -1,20 +1,22 @@
 import loginTest from './login.cy';
 
-
 describe('Login Tests', () => {
     beforeEach(() => {  
-        cy.visit('https://dashboard.dev.transax.com/auth/login', { 
-            failOnStatusCode: false 
-        });
+        cy.visit('https://dashboard.dev.transax.com/auth/login', { failOnStatusCode: false });
     });
     
     it('should login successfully', () => {
-        loginTest.login('jacksmith4@transax.com', 'Qatest111!');
+        const username = Cypress.env('username');
+        const password = Cypress.env('password');
+        
+        // Verify environment variables are loaded
+        expect(username, 'Username should be defined').to.not.be.undefined;
+        expect(password, 'Password should be defined').to.not.be.undefined;
+        
+        loginTest.login(username, password);
     });
 
-    it('should show error message for invalid login credentials', () => {
-        loginTest.invalidLogin('invaliduser', 'invalidpass');
-    });
+   
 });
 
 
